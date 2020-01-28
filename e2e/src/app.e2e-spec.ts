163dae7136d24db4746fbfbc659856d84d2bf71b
Mikko Ohtamaa <mikko@opensourcehacker.com>
2020-01-28 22:30:43 +0100

    Added a demostration of E2E tests we are looking to see

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -5,22 +5,28 @@
  */
 
 import { browser, logging, element, by } from 'protractor';
-import { fillField } from './utils';
+import { fillMaterialField } from './utils';
 
 describe('workspace-project App', () => {
 
   beforeEach(() => {
   });
 
-  it('should display a sign up form', async () => {
+  it('should display a sign up form on the landing page', async () => {
     await browser.get(browser.baseUrl);
     const form = element(by.css('.form-signup'));
-    expect(form.isPresent());
+    expect(form.isPresent()).toBe(true);
   });
 
   it('should arrive to the user dashboard after sign up', async () => {
     await browser.get(browser.baseUrl);
-    await fill
+    const name = "Mikko Ohtamaa";
+    
+    await fillMaterialField("password", "supersecret");
+    await fillMaterialField("email", "example@example.com"); // https://en.wikipedia.org/wiki/Example.com
+    await fillMaterialField("name", name)
+    await element(by.css('.form-signup button')).click();
+    expect(element(by.css('.span-user-full-name')).getText()).toBe(name);
   });  
 
   afterEach(async () => {
--- e2e/src/utils.ts
+++ e2e/src/utils.ts
@@ -1,4 +1,4 @@
-import { browser, logging, element, by } from 'protractor';
+import { browser, logging, element, by, ExpectedConditions } from 'protractor';
 
 // https://stackoverflow.com/questions/50970567/more-efficient-way-to-fill-out-my-form-in-protractor/50970938
 export async function fillInput(el, text) {
@@ -11,7 +11,20 @@ export async function fillInput(el, text) {
  * Fills a named field on a form.
  */
 export async function fillField(name, text) {
-    const el = element(by.css(`input[name='{name}']`));
-    expect(el.isPresent());
+    const el = element(by.css(`input[name='${name}']`));    
+    expect(el.isPresent()).toBe(true);
     fillInput(el, text);
+}
+
+/**
+ * Angular Material inputs are named specially and they do not honour name attribute.
+ * 
+ * <input _ngcontent-ocw-c5="" class="mat-input-element mat-form-field-autofill-control cdk-text-field-autofill-monitored ng-pristine ng-invalid ng-touched" formcontrolname="email" matinput="" placeholder="example@example.com" ng-reflect-name="email" ng-reflect-placeholder="example@example.com" id="mat-input-0" aria-invalid="true" aria-required="false">
+ * 
+ */
+export async function fillMaterialField(name, text) {
+    const selector = by.css(`input[formcontrolname='${name}']`);
+    const el = element(selector);    
+    expect(await el.isPresent()).toBe(true);
+    await fillInput(el, text);
 }
